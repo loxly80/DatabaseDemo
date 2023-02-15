257c65d028d1d1c815190e6ba3f9efce7a3895ef
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2023-02-15 09:43:18 +0100

    vytvoření třídy sqlRepo pro zapouzdření akcí s DB

--- DatabaseDemo/Form1.cs
+++ DatabaseDemo/Form1.cs
@@ -4,41 +4,22 @@ namespace DatabaseDemo
 {
   public partial class Form1 : Form
   {
-    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Demo;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    private SqlRepo sqlRepo;
 
     public Form1()
     {
       InitializeComponent();
+      sqlRepo= new SqlRepo();
     }
 
     private void Form1_Load(object sender, EventArgs e)
     {
-      LoadData();
-    }
-
-    private void LoadData()
-    {
-      using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+      foreach(var row in sqlRepo.LoadData())
       {
-        using (SqlCommand command = sqlConnection.CreateCommand())
-        {
-          command.CommandText = "select IdDemo, Col1, Col2, Col3 from Demo";
-          sqlConnection.Open();
-          using (SqlDataReader reader = command.ExecuteReader())
-          {
-            while (reader.Read())
-            {
-              //naètení hodnot do pole øetìzcù
-              string[] data = new string[] { reader[0].ToString(), reader[1].ToString(), reader[2].ToString(), reader[3].ToString() };
-              //vytvoøení položky pro ListView
-              ListViewItem item = new ListViewItem(data);
-              //vložení do ListView
-              listView1.Items.Add(item);
-            }
-          }
-          sqlConnection.Close();
-        }
+        listView1.Items.Add(new ListViewItem(row));
       }
     }
+
+    
   }
 }
---
+++ DatabaseDemo/SqlRepo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseDemo
+{
+  public class SqlRepo
+  {
+    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Demo;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+    public List<string[]> LoadData()
+    {
+      List<string[]> data = new List<string[]>();
+      using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+      {
+        using (SqlCommand command = sqlConnection.CreateCommand())
+        {
+          command.CommandText = "select IdDemo, Col1, Col2, Col3 from Demo";
+          sqlConnection.Open();
+          using (SqlDataReader reader = command.ExecuteReader())
+          {
+            while (reader.Read())
+            {
+              //načtení hodnot do pole řetězců
+              string[] row = new string[] { reader[0].ToString(), reader[1].ToString(), reader[2].ToString(), reader[3].ToString() };
+              data.Add(row);
+            }
+          }
+          sqlConnection.Close();
+        }
+      }
+      return data;
+    }
+  }
+}
